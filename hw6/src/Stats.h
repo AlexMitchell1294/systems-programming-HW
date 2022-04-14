6914fac14fa82c6fc7d81924adf206ebd9f1ed3d
AlexMitchell1294 <73563901+AlexMitchell1294@users.noreply.github.com>
2022-04-14 15:35:26 -0400

    Add files via upload

---
+++ hw6/src/Stats.h
@@ -0,0 +1,21 @@
+/*
+ * Stats.h
+ *
+ *  Created on: Oct 9, 2021
+ *      Author: darth
+ */
+
+#ifndef STATS_H_
+#define STATS_H_
+
+
+class Stats{
+public:
+	int numHits;
+	int numMisses;
+	int totalShots;
+	double hitMissRatio;
+};
+
+
+#endif /* STATS_H_ */
